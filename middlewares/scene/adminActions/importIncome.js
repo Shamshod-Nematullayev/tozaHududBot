4286a30eadb8cd221f6d5425f5e0a0c6f7a36a6a
Пользователь <maxxadsen@gmail.com>
2023-04-26 17:54:12 +0500

    tushum

--- middlewares/scene/adminActions/importIncome.js
+++ middlewares/scene/adminActions/importIncome.js
@@ -4,28 +4,63 @@ const { messages } = require("../../../lib/messages");
 const { Abonent } = require("../../../models/YangiAbonent");
 const isCancel = require("../../smallFunctions/isCancel");
 const qaysiMahalla = require("../../smallFunctions/qaysiMahalla");
+const excelToJson = require("convert-excel-to-json");
+const https = require("https");
+const fs = require("fs");
 
 const importIncomeScene = new Scenes.WizardScene(
   "import_income_report",
-  (ctx) => {
+  async (ctx) => {
+    console.log(ctx.message.document);
+    if (isCancel(ctx.message.text)) return ctx.scene.leave();
     if (
       ctx.message.document &&
       (ctx.message.document.mime_type ==
         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
         ctx.message.document.mime_type == "application/vnd.ms-excel")
     ) {
+      // Agar nazoratchilar kesimida xisobot bo'lsa
+      if (
+        ctx.message.document.mime_type ==
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      ) {
+        const xlsx = await ctx.telegram.getFileLink(
+          ctx.message.document.file_id
+        );
+        const excelFile = fs.createWriteStream("./incomeReport.xls");
+        https.get(xlsx.href, (res) => {
+          res.pipe(excelFile);
+          excelFile.on("finish", (cb) => {
+            excelFile.close(cb);
+            const xls = excelToJson({
+              sourceFile: "./incomeReport.xls",
+            });
+            const sheet = xls[Object.keys(xls)[0]];
+            const newJSON = {};
+            newJSON.sana = sheet[0].A;
+            newJSON.dan = sheet[2].B;
+            newJSON.gacha = sheet[3].A;
+            newJSON.nazoratchilar = sheet.slice(7, sheet.length - 1);
+          });
+        });
+      }
       ctx.wizard.state.file_id = ctx.message.document.file_id;
       ctx.reply(
         messages.lotin.chooseIncomeType,
         keyboards.lotin.chooseIncomeType
       );
-      ctx.wizard.next();
+      // ctx.wizard.next();
     } else {
       ctx.reply(messages.lotin.notExcelFile);
     }
   },
   (ctx) => {
-    console.log(ctx.update);
+    try {
+      if (ctx.message && isCancel(ctx.message.text)) return ctx.scene.leave();
+      console.log(ctx.update);
+    } catch (error) {
+      console.log(error);
+    }
   }
 );
 
@@ -39,7 +74,9 @@ importIncomeScene.enter((ctx) => {
 importIncomeScene.leave((ctx) => {
   ctx.reply(
     messages[ctx.session.til ? ctx.session.til : "lotin"].startGreeting,
-    keyboards[ctx.session.til ? ctx.session.til : "lotin"].mainKeyboard.resize()
+    keyboards[
+      ctx.session.til ? ctx.session.til : "lotin"
+    ].adminKeyboard.resize()
   );
 });
 
