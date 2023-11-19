bfd5786e9a2d69723d4e90f18b237050806897a1
Shamshod Nematullayev <maxxadsen@gmail.com>
2023-11-19 12:28:50 +0500

    kutubxonadagi keyboardlar ko'rinishi soddaroq holga keltirildi va createInlineKeyboard funksiyasi qoshildi

--- actions/language.js
+++ actions/language.js
@@ -5,12 +5,14 @@ const { messages } = require("../lib/messages");
 
 const composer = new Composer();
 composer.action("language", (ctx) => {
-  ctx.reply(
+  ctx.deleteMessage();
+  return ctx.reply(
     messages[ctx.session.til].chooseLanguage,
     keyboards[ctx.session.til].chooseLanguge
   );
 });
 composer.action("lotin_tili_tanlash", (ctx) => {
+  ctx.deleteMessage();
   ctx.session.til = "lotin";
   ctx.reply(
     messages[ctx.session.til].choosedLang,
@@ -18,6 +20,7 @@ composer.action("lotin_tili_tanlash", (ctx) => {
   );
 });
 composer.action("kiril_tili_tanlash", (ctx) => {
+  ctx.deleteMessage();
   ctx.session.til = "kiril";
   ctx.reply(
     messages[ctx.session.til].choosedLang,
