14eaca3e346b945ca3f18b81f9dc7edc0f961f9e
Пользователь <maxxadsen@gmail.com>
2023-12-28 15:53:52 +0500

    yangi dxsh papkasi

---
+++ middlewares/scene/adminActions/cleancity/dxsh/mfyIncomeReport.js
@@ -0,0 +1,187 @@
+// ==============================================================
+const { Composer } = require("telegraf");
+const { bot } = require("../../../../../core/bot");
+const { CleanCitySession } = require("../../../../../models/CleanCitySession");
+const { JSDOM } = require("jsdom");
+const { Mahalla } = require("../../../../../models/Mahalla");
+const ejs = require("ejs");
+const nodeHtmlToImage = require("node-html-to-image");
+const path = require("path");
+const fs = require("fs");
+
+// ==============================================================
+// MAIN FUNCTION
+
+const composer = new Composer();
+
+const mfyIncomeReport = async (ctx = false) => {
+  const sessions = await CleanCitySession.find({
+    type: "dxsh",
+    active: true,
+  });
+
+  //   Foydalanuvchi tomonidan kiritilgan va hozirda aktiv bo'lgan sessionlar topilmaganda session yaratish sahnasiga o'tkazib yuboriladi
+  if (sessions.length < 1 && ctx) {
+    await ctx.reply(`Faol sessiyalar mavjud emas qaytadan login qiling`);
+    // session yaratish sahnasi va callback function shu joyida tugab yangi sahna boshlanadi
+    await ctx.scene.enter("login_clean_city");
+    return;
+  }
+  //   foydalanuvchi kiritgan har bir session bo'yicha tushumlar tahlili qilinadi
+  sessions.forEach(async (session) => {
+    // bosh sahifani yuklab olish
+    const resHomePage = await fetch(`https://cleancity.uz/startpage`, {
+      headers: {
+        Cookie: session.cookie,
+      },
+    });
+    // Tushumlar tahliliga o'tkazadigan linkni aniqlash
+    const textHomePage = await resHomePage.text();
+    const docHomePage = new JSDOM(textHomePage).window.document;
+    // Mana o'sha link => tushumlar tahlili sahifasiga
+    if (
+      !docHomePage.querySelector(
+        "#g_acccordion > div > div:nth-child(5) > ul > li:nth-child(4) > a"
+      ) &&
+      ctx
+    ) {
+      await ctx.reply(`Faol sessiyalar mavjud emas qaytadan login qiling`);
+      // session yaratish sahnasi va callback function shu joyida tugab yangi sahna boshlanadi
+      ctx.session.session_type = "dxsh";
+      await ctx.scene.enter("recover_clean_city_session");
+      return;
+    }
+    const jamiTushumlarTahliliPath = docHomePage.querySelector(
+      "#g_acccordion > div > div:nth-child(5) > ul > li:nth-child(4) > a"
+    ).href;
+
+    // Tushumlar tahlili sahifasini yuklab olish
+    const resTushumlarTahlili = await fetch(
+      `https://cleancity.uz/startpage` + jamiTushumlarTahliliPath,
+      {
+        headers: {
+          Cookie: session.cookie,
+        },
+      }
+    );
+    // Ma'lumotlarni json shaklida beradigan linkni aniqlash
+    const textTushumlarTahliliPage = await resTushumlarTahlili.text();
+    const url = textTushumlarTahliliPage
+      .match(/url:\s*'([^']+)'/g)[0]
+      .split("'")[1];
+    let myHeaders = new Headers();
+    myHeaders.append("Cookie", session.cookie);
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    let urlencoded = new URLSearchParams();
+    const date = new Date();
+    urlencoded.append("mes", date.getMonth() + 1);
+    urlencoded.append("god", date.getFullYear());
+    urlencoded.append("companies_id", "1144");
+    urlencoded.append("sort", "id");
+    urlencoded.append("order", "desc");
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+    fetch("https://cleancity.uz/" + url, requestOptions)
+      .then((response) => response.json())
+      .then(async (result) => {
+        // linkni mongodb sessionga qo'shib qo'yish keyinchalik ishlatish uchun
+        await session.updateOne({ $set: { "path.getIncomes": url } });
+        // tushumlar tahlilini telegram guruhlarga yuborish
+        const mahallalar = [];
+        let i = 0;
+        // mahallalar ma'lumotlarini birlashtirib bitta arrayga olish
+        const func = async () => {
+          const row = result.rows[i];
+          const mahalla = await Mahalla.findOne({ id: row.id });
+          if (mahalla && mahalla.reja > 0) {
+            mahallalar.push({
+              id: row.id,
+              xisoblandi: mahalla.reja,
+              name: row.name,
+              tushum: Number(row.ab_all_amount),
+              nazoratchi: mahalla.biriktirilganNazoratchi,
+            });
+          }
+          i++;
+          if (i < result.rows.length) await func();
+        };
+        await func();
+
+        // Tayyor array bilan hisobotni chizish
+        let jamiXisoblandi = 0;
+        let jamiTushum = 0;
+        mahallalar.sort(
+          (a, b) =>
+            parseFloat(b.tushum / b.xisoblandi) -
+            parseFloat(a.tushum / a.xisoblandi)
+        );
+        mahallalar.forEach((mfy, index) => {
+          jamiXisoblandi += mfy.xisoblandi;
+          jamiTushum += mfy.tushum;
+        });
+        const sana = `${date.getDate()} ${
+          date.getMonth() + 1
+        } ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+        ejs.renderFile(
+          path.join(__dirname, `../../../../views/mfyIncome.ejs`),
+          { data: mahallalar, jamiTushum, jamiXisoblandi, sana },
+          {},
+          async (err, str) => {
+            if (err) {
+              ctx.reply("Hisobot render qilishda xatolik");
+              return console.log(err);
+            }
+            nodeHtmlToImage({
+              output: "./income.png",
+              html: str,
+              type: "png",
+              encoding: "binary",
+              selector: "div",
+            })
+              .then(() => {
+                if (ctx)
+                  ctx.replyWithPhoto({ source: "./income.png" }).then(() => {
+                    fs.unlink("./income.png", (err) => {
+                      if (err) throw err;
+                    });
+                  });
+                else
+                  bot.telegram
+                    .sendPhoto(
+                      process.env.NAZORATCHILAR_GURUPPASI,
+                      { source: "./income.png" },
+                      {
+                        caption: `Coded by <a href="https://t.me/oliy_ong_leader">Oliy Ong</a>`,
+                        parse_mode: "HTML",
+                      }
+                    )
+
+                    .then(() => {
+                      fs.unlink("./income.png", (err) => {
+                        if (err) throw err;
+                      });
+                    });
+              })
+              .catch((err) => {
+                if (ctx) ctx.reply("Hisobotni rasmga o'girishda xatolik");
+                console.log(err);
+              });
+          }
+        );
+      })
+      .catch((error) => {
+        // xatolik kuzatilsa foydalanuvchi va developerga yetkazish
+        console.log("error", error);
+        ctx.reply("xatolik tushumlar ma'lumotini json formatida olishda");
+      });
+  });
+};
+
+// =========== EXPORT ======================//
+composer.action("mfy_income_report", mfyIncomeReport);
+module.exports = { mfyIncomeReport };
+bot.use(composer);
---
+++ middlewares/scene/adminActions/cleancity/dxsh/yangiAbonent.js
@@ -0,0 +1,122 @@
+const cc = `https://cleancity.uz/`;
+const { CleanCitySession } = require("../../../../../models/CleanCitySession");
+const { JSDOM } = require("jsdom");
+
+// ========================================================================
+
+const yangiAbonent = async ({
+  mfy_id,
+  fish,
+  street_id,
+  yashovchi_soni,
+  kadastr_number,
+  phone,
+  passport_number,
+  pinfl,
+}) => {
+  const date = new Date();
+  try {
+    const session = await CleanCitySession.findOne({ type: "dxsh" });
+    if (!session) {
+      return { success: false, msg: "Session not available" };
+    }
+
+    let new_abonent_url = session.path.get_litschet_url;
+    let get_litschet_url = session.path.new_abonent_url;
+
+    if (!new_abonent_url || !get_litschet_url) {
+      const startpage = await fetch(cc + "startpage", {
+        headers: { Cookie: session.cookie },
+      });
+      const startpageText = await startpage.text();
+
+      // let url = startpageText.match(/href="?x=([^"]+)'/g);
+      // console.log(url);
+      const startpageDoc = new JSDOM(startpageText).window.document;
+      let find_abonents_page_url = startpageDoc
+        .querySelector(
+          "#g_acccordion > div > div:nth-child(1) > ul > li:nth-child(1) > a"
+        )
+        .getAttribute("href");
+      const abonents_page_url = await fetch(cc + find_abonents_page_url, {
+        headers: { Cookie: session.cookie },
+      });
+      const abonents_page_res = await fetch(abonents_page_url.url, {
+        headers: { Cookie: session.cookie },
+      });
+      const abonents_page_text = await abonents_page_res.text();
+      new_abonent_url = abonents_page_text
+        .match(/url\s*=\s*'([^']+)'/g)[2]
+        .split("'")[1]; //uchinchi topilgan url yangi abonent ochish yo'li
+
+      // return;
+      get_litschet_url = abonents_page_text
+        .match(/\$\.post\(\'ds\?xenc=([^'";\s]+)/g)[9]
+        .split("'")[1]; //[3].split("'")[1]; //uchinchi topilgan url yangi abonent ochish yo'li
+    }
+    let availableKOD = await fetch(cc + get_litschet_url, {
+      method: "POST",
+      headers: {
+        accept: "application/json, text/javascript, */*; q=0.01",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Cookie: session.cookie,
+      },
+      body: `mahallas_id=${mfy_id}&companies_id=1144`,
+    });
+    availableKOD = await availableKOD.json();
+    // return;
+    let finalResponse = await fetch("https://cleancity.uz/" + new_abonent_url, {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,uz;q=0.7",
+        "cache-control": "max-age=0",
+        "content-type": "application/x-www-form-urlencoded",
+        "sec-ch-ua":
+          '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "iframe",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        Cookie: session.cookie,
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: `id=&fio=${fish}&prescribed_cnt=${yashovchi_soni}&mahallas_id=${mfy_id}&licshet=${
+        availableKOD.value
+      }&streets_id=${street_id}&kadastr_number=${
+        availableKOD.value /*kadastr_number*/
+      }&headquarters_id=&ind=&house=&flat=&house_type_id=1&inn=&contract_number=&contract_date=${
+        date.getDate() > 9 ? date.getDate() : "0" + date.getDate()
+      }.${
+        date.getMonth() + 1 > 9
+          ? date.getMonth() + 1
+          : "0" + date.getMonth() + 1
+      }.${date.getFullYear()}&energy_licshet=&energy_coato=&phone=&home_phone=&email=&description=Added from telegram bot&passport_location=&passport_number=${passport_number}&brith_date=&passport_given_date=&passport_expire_date=&pinfl=${pinfl}`,
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    });
+
+    const final = await finalResponse.json();
+    console.log({ final });
+    // if (final.success) {
+    //   await session.updateOne({
+    //     $set: {
+    //       "path.new_abonent_url": new_abonent_url,
+    //       "path.get_litschet_url": get_litschet_url,
+    //     },
+    //   });
+    // }
+    return {
+      litschet: availableKOD.value,
+      ...final,
+    }; // Return the 'final' variable
+  } catch (err) {
+    console.error(err.message);
+    throw err;
+  }
+};
+module.exports = { yangiAbonent };
