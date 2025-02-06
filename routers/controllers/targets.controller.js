1590544c2fda08fb7c8c8bca9e04ce047514f8ed
Shamshod Nematullayev <maxxadsen@gmail.com>
2025-02-06 12:24:29 +0500

    /targets/createDocument /targets/signDocument/:document_id endpointes created

--- routers/controllers/targets.controller.js
+++ routers/controllers/targets.controller.js
@@ -1,3 +1,5 @@
+const { bot } = require("../../core/bot");
+const { Bildirishnoma } = require("../../models/SudBildirishnoma");
 const { Target } = require("../../models/TargetAbonent");
 
 module.exports.getTargets = async (req, res) => {
@@ -29,6 +31,76 @@ module.exports.getTargets = async (req, res) => {
         limit: limit,
         totalPages: Math.ceil(totalCount / limit),
       },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ ok: false, message: "Internal Server Error" });
+  }
+};
+
+module.exports.createDocumentTargets = async (req, res) => {
+  try {
+    const { targets, abonents, inspector, mahallaId } = req.body;
+    if (!targets || targets.length === 0) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Targets are required" });
+    }
+    const lastDocument = await Bildirishnoma.findOne({
+      type: "sudga_chiqoring",
+    }).sort({ doc_num: -1 });
+    const doc_num = lastDocument ? lastDocument.doc_num + 1 : 1;
+    const document = await Bildirishnoma.create({
+      type: "sudga_chiqoring",
+      doc_num,
+      abonents,
+      targets,
+      inspector: {
+        name: inspector.name,
+        id: inspector.id,
+      },
+      date: new Date(),
+      mahallaId,
+      user: {
+        _id: req.user.id,
+        username: req.user.login,
+      },
+    });
+    res
+      .status(201)
+      .json({ ok: true, message: "Targets created successfully", document });
+    targets.forEach(async (target) => {
+      await Target.findByIdAndUpdate(target, {
+        $set: { document_id: document._id, status: "xujjat_yaratildi" },
+      });
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ ok: false, message: "Internal Server Error" });
+  }
+};
+
+module.exports.signDocumentTargets = async (req, res) => {
+  try {
+    const { document_id } = req.params;
+    // upload file to telegram
+    const ctx = await bot.telegram.sendDocument(process.env.CHANNEL, {
+      source: req.file.buffer,
+    });
+    // updateDocument
+    const document = await Bildirishnoma.findByIdAndUpdate(document_id, {
+      $set: { file_id: ctx.document.file_id },
+    });
+    // res.send
+    res.status(200).json({
+      ok: true,
+      message: "Document signed successfully",
+    });
+    // updateTargets
+    document.targets?.forEach(async (target) => {
+      await Target.findByIdAndUpdate(target, {
+        $set: { status: "tasdiqlandi" },
+      });
     });
   } catch (error) {
     console.error(error.message);
--- routers/targetsRouter.js
+++ routers/targetsRouter.js
@@ -1,7 +1,20 @@
 const { Router } = require("express");
-const { getTargets } = require("./controllers/targets.controller");
+const {
+  getTargets,
+  createDocumentTargets,
+  signDocumentTargets,
+} = require("./controllers/targets.controller");
+const { uploadAsBlob } = require("../middlewares/multer");
 const router = Router();
 
 router.get("/", getTargets);
+
+router.post("/createDocument", createDocumentTargets);
+
+router.patch(
+  "/signDocument/:document_id",
+  uploadAsBlob.single("file"),
+  signDocumentTargets
+);
 
 module.exports = router;
