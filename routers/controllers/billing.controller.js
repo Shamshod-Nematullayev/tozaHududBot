eec25ff0b6204585b1e3ea15aaae8f0dbda4c3f2
Shamshod Nematullayev <maxxadsen@gmail.com>
2025-02-07 23:39:57 +0500

    billingdan faylni yuklab olib base64 qilib yuborish fuksiyasi ishga tayyor

---
+++ routers/controllers/billing.controller.js
@@ -0,0 +1,35 @@
+const { tozaMakonApi } = require("../../api/tozaMakon");
+
+module.exports.downloadFileFromBilling = async (req, res) => {
+  try {
+    const { file_id } = req.query;
+    if (!file_id) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Fayl ID talab qilinadi." });
+    }
+
+    const cleanFileId = file_id.split("*").pop(); // Oxirgi qismni olish
+
+    // Server 2 ga so‘rov yuborish
+    const response = await tozaMakonApi.get("/file-service/buckets/download", {
+      params: { file: cleanFileId },
+      responseType: "arraybuffer",
+    });
+    // Faylni Base64 ga o'tkazish
+    const base64Data = Buffer.from(response.data).toString("base64");
+    const contentType = "application/pdf";
+
+    res.json({
+      ok: true,
+      file: `data:${contentType};base64,${base64Data}`,
+    });
+  } catch (error) {
+    console.error("Error downloading file:", error);
+
+    res.status(500).json({
+      ok: false,
+      message: error.response?.data || error.message || "Internal server error",
+    });
+  }
+};
