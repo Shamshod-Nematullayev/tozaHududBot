3fe76b91756e93eda888a87999f283dadafc2fd4
Shamshod Nematullayev <maxxadsen@gmail.com>
2025-03-10 22:52:59 +0500

    buzilgan aktlarni tuzatish uchun seed command

--- test.js
+++ test.js
@@ -6,10 +6,11 @@
 // const { Nazoratchi, keyboards } = require("./requires");
 // const { lotinga } = require("./middlewares/smallFunctions/lotinKiril");
 
-const { Abonent } = require("./requires");
+const { Abonent, Company } = require("./requires");
 
 const { tozaMakonApi } = require("./api/tozaMakon");
 const { SudAkt } = require("./models/SudAkt");
+const { Ariza } = require("./models/Ariza");
 
 let init = 50000;
 // Abonent.find().then((abonents) => {
@@ -213,5 +214,34 @@ const func = async () => {
 (async () => {
   const rows = require("./main.json");
   for (let row of rows) {
+    const ariza = await Ariza.findOne({ akt_id: row.id });
+    const abonent = await Abonent.findOne({ licshet: ariza.licshet });
+    const company = await Company.findOne({ id: 1144 });
+    const amounts = (
+      await tozaMakonApi.get("/billing-service/acts/calculate-amount", {
+        params: {
+          actPackId: company.akt_pachka_ids.gps.id,
+          residentId: abonent.id,
+          inhabitantCount: 0,
+          kSaldo: 0,
+          startPeriod: "03.2025",
+          endPeriod: "03.2025",
+        },
+      })
+    ).data;
+    await tozaMakonApi.post("/billing-service/acts", {
+      actPackId: company.akt_pachka_ids.gps.id,
+      actType: amounts.actType,
+      amount: amounts.amount,
+      amountWithQQS: amounts.amountWithQQS,
+      amountWithoutQQS: 0,
+      description: `kamchiliklarni bartaraf etish`,
+      startPeriod: "03.2025",
+      endPeriod: "03.2025",
+      fileId: ariza.aktInfo.fileId,
+      kSaldo: 0,
+      residentId: abonent.id,
+      inhabitantCounts: 0,
+    });
   }
 })();
