3c83830fb06f3a6d10330aba8cf21e68b7b7f617
Shamshod Nematullayev <maxxadsen@gmail.com>
2025-03-18 13:04:23 +0500

    socket.io configuratsiya qilindi

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -3,12 +3,30 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const server = http.createServer(app);
+const jwt = require("jsonwebtoken");
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:8000",
+    credentials: true,
+  },
+});
 
 const usersMap = {};
 io.on("connection", (socket) => {
-  console.log("a user connected", socket);
+  try {
+    const decoded = jwt.verify(
+      socket.handshake.query.accessToken || "",
+      process.env.SECRET_JWT_KEY
+    );
+    usersMap[decoded.id] = socket.id;
+
+    socket.on("disconnect", () => {
+      delete usersMap[decoded.id];
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 module.exports = { io, app, server };
