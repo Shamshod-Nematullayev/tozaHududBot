df555a1a5c019e2a2bc704d9f9676113ca506fc6
Shamshod Nematullayev <maxxadsen@gmail.com>
2025-04-26 18:16:24 +0500

    sud ko'p kompaniyali qilinmadi hali

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.use(
 // use routers
 app.use("/api/auth", require("./routers/auth"));
 app.use("/api/statistics", isAuth, require("./routers/statisticsRouter"));
-app.use("/api/court-service", isAuth, require("./routers/sudRouter"));
-app.use("/api/targets", isAuth, require("./routers/targetsRouter"));
-app.use("/api/bildirgilar", isAuth, require("./routers/bildirgilarRouter"));
+// app.use("/api/court-service", isAuth, require("./routers/sudRouter"));
+// app.use("/api/targets", isAuth, require("./routers/targetsRouter"));
+// app.use("/api/bildirgilar", isAuth, require("./routers/bildirgilarRouter"));
 app.use("/api/fetchTelegram", isAuth, require("./routers/fetchTelegramRouter"));
 app.use("/api/pachkalar", isAuth, require("./routers/aktPachka"));
 app.use("/api/documents", isAuth, require("./routers/kiruvchiXujjatlar"));
@@ -37,7 +37,7 @@ app.use("/api/inspectors", isAuth, require("./routers/inspectorsRouter"));
 app.use("/api/abonents", isAuth, require("./routers/abonentsRouter"));
 app.use("/api/billing", isAuth, require("./routers/billing"));
 app.use("/api/arizalar", isAuth, require("./routers/arizalarRouter"));
-app.use("/api/notification", isAuth, require("./routers/notificationRouter"));
+// app.use("/api/notification", isAuth, require("./routers/notificationRouter"));
 app.use(
   "/api/yashovchi-soni-xatlov",
   isAuth,
