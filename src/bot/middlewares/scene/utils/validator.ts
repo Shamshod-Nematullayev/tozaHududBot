084e24b9e2ae6a89e5843f76da51af0067c30939
Shamshod <maxxadsen@gmail.com>
2025-07-20 08:53:45 +0500

    .

--- src/bot/middlewares/scene/utils/validator.ts
+++ src/bot/middlewares/scene/utils/validator.ts
@@ -1,3 +1,10 @@
+import { Context } from "telegraf";
+import { Message } from "telegraf/typings/core/types/typegram";
+
 export function isValidAccountNumber(text: string): boolean {
   return /^\d{12,}$/.test(text);
+}
+
+export function isTextMessage(ctx: Context): boolean {
+  return Boolean(ctx.message && "text" in ctx.message);
 }
