4a98623b443610024501da6328004c1d8ab2c8cf
Shamshod-Nematullayev <maxxadsen@gmail.com>
2025-11-04 21:38:05 +0500

    chala

--- src/services/gps/getGeozonesFromSmartGps.ts
+++ src/services/gps/getGeozonesFromSmartGps.ts
@@ -1,16 +1,16 @@
-import { Axios } from "axios";
+// import { Axios } from "axios";
 
-export async function getGeozonesFromSmartGps(smartGpsApi: Axios, ): Promise<any[]> {
-    let itemId //
-    const res = await smartGpsApi.post('/wialon/ajax.html?svc=core/batch', {
-        params: JSON.stringify({
-            svc: "core/batch",
-            params: {
-                svc: "resource/get_notification_data",
-                params: {
-                    itemId: 
-                }
-            }
-        })
-    })
-}
+// export async function getGeozonesFromSmartGps(smartGpsApi: Axios, ): Promise<any[]> {
+//     let itemId //
+//     const res = await smartGpsApi.post('/wialon/ajax.html?svc=core/batch', {
+//         params: JSON.stringify({
+//             svc: "core/batch",
+//             params: {
+//                 svc: "resource/get_notification_data",
+//                 params: {
+//                     itemId:
+//                 }
+//             }
+//         })
+//     })
+// }HALI chala funksiya
