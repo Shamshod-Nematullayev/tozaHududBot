df2b43861043b1abed98e6d5f69f53dbc6788f6e
Shamshod Nematullayev <maxxadsen@gmail.com>
2026-02-04 09:21:31 +0500

    update backend logic

--- src/config/jobsConfig.ts
+++ src/config/jobsConfig.ts
@@ -3,42 +3,19 @@ import { sendKunlikPinflReports } from "intervals/kunlikPinflReports.js";
 import { mahallaTushumlarNazoratchiKesimida } from "intervals/mahallaTushumlarNazoratchiKesimida.js";
 import { sendEtkMfyReport } from "intervals/sendEtkMfyReport.js";
 import { sendMFYIncomeReport } from "intervals/sendMFYIncomeReport.js";
+import { specialTaskReport } from "intervals/specialTaskReport.js";
 import xatlovchilarIshiHisobot from "intervals/xatlovchilarIshiHisobot.js";
 
 interface IJobConfig {
-  name:
-    | "sendKunlikPinflReportsTask"
-    | "sendKunlikEtkReportsTask"
-    | "sendMFYIncomeReportTask"
-    | "nazoratchilarKunlikTushumTask";
+  name: string;
   schedule: string;
   handler: () => Promise<void>;
 }
 
 export const jobsConfig: IJobConfig[] = [
   {
-    name: "sendKunlikPinflReportsTask",
-    schedule: "15 9-22 * * *",
-    handler: async () => {
-      // parametrlar oson o'zgarishi uchun massiv ishlating
-      console.log("nahotki");
-      xatlovchilarIshiHisobot(1144);
-    },
-  },
-  {
-    name: "sendKunlikEtkReportsTask",
-    schedule: "0 9,11,13,15,17,19,21,23 * * *",
-    handler: async () => {
-      const companyIds = [621];
-      for (const id of companyIds) {
-        await sendKunlikEtkReports(id);
-        await sendKunlikPinflReports(id);
-      }
-    },
-  },
-  {
     name: "sendMFYIncomeReportTask",
-    schedule: "0 9 * * *",
+    schedule: "0 9,12,17,19 * * *",
     handler: async () => {
       await sendMFYIncomeReport(1144, false, false, true);
       await sendMFYIncomeReport(1824, false, false, false);
@@ -46,7 +23,7 @@ export const jobsConfig: IJobConfig[] = [
   },
   {
     name: "nazoratchilarKunlikTushumTask",
-    schedule: "0 9-22 * * *",
+    schedule: "0 9,12,15,17,20 * * *",
     handler: async () => {
       await mahallaTushumlarNazoratchiKesimida({
         companyId: 1144,
@@ -60,9 +37,14 @@ export const jobsConfig: IJobConfig[] = [
         to: new Date(),
         shouldDeleteLastReport: false,
       });
-
-      await sendKunlikEtkReports(1144);
-      await sendEtkMfyReport(1144);
+    },
+  },
+  {
+    name: "maxsusTopshiriq",
+    schedule: "0 13,19, * * *",
+    handler: async () => {
+      await specialTaskReport(1144, "phone");
+      await specialTaskReport(1144, "electricity");
     },
   },
 ];
