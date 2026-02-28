e2d03298ee390deedf1399d8145c223b97347720
Shamshod Nematullayev <maxxadsen@gmail.com>
2026-02-28 10:01:36 +0500

    endi telefon raqami bo'yicha maxsus topshiriq hisoboti kerak emas

--- src/config/jobsConfig.ts
+++ src/config/jobsConfig.ts
@@ -1,10 +1,10 @@
-import { sendKunlikEtkReports } from "intervals/kunlikEtkReports.js";
-import { sendKunlikPinflReports } from "intervals/kunlikPinflReports.js";
-import { mahallaTushumlarNazoratchiKesimida } from "intervals/mahallaTushumlarNazoratchiKesimida.js";
-import { sendEtkMfyReport } from "intervals/sendEtkMfyReport.js";
-import { sendMFYIncomeReport } from "intervals/sendMFYIncomeReport.js";
-import { specialTaskReport } from "intervals/specialTaskReport.js";
-import xatlovchilarIshiHisobot from "intervals/xatlovchilarIshiHisobot.js";
+import { sendKunlikEtkReports } from 'intervals/kunlikEtkReports.js';
+import { sendKunlikPinflReports } from 'intervals/kunlikPinflReports.js';
+import { mahallaTushumlarNazoratchiKesimida } from 'intervals/mahallaTushumlarNazoratchiKesimida.js';
+import { sendEtkMfyReport } from 'intervals/sendEtkMfyReport.js';
+import { sendMFYIncomeReport } from 'intervals/sendMFYIncomeReport.js';
+import { specialTaskReport } from 'intervals/specialTaskReport.js';
+import xatlovchilarIshiHisobot from 'intervals/xatlovchilarIshiHisobot.js';
 
 interface IJobConfig {
   name: string;
@@ -14,16 +14,16 @@ interface IJobConfig {
 
 export const jobsConfig: IJobConfig[] = [
   {
-    name: "sendMFYIncomeReportTask",
-    schedule: "0 9,12,17,19 * * *",
+    name: 'sendMFYIncomeReportTask',
+    schedule: '0 9,12,17,19 * * *',
     handler: async () => {
       await sendMFYIncomeReport(1144, false, false, true);
       await sendMFYIncomeReport(1824, false, false, false);
     },
   },
   {
-    name: "nazoratchilarKunlikTushumTask",
-    schedule: "0 9,12,15,17,20 * * *",
+    name: 'nazoratchilarKunlikTushumTask',
+    schedule: '0 9,12,15,17,20 * * *',
     handler: async () => {
       await mahallaTushumlarNazoratchiKesimida({
         companyId: 1144,
@@ -40,11 +40,10 @@ export const jobsConfig: IJobConfig[] = [
     },
   },
   {
-    name: "maxsusTopshiriq",
-    schedule: "0 9,10,13,17,19 * * *",
+    name: 'maxsusTopshiriq',
+    schedule: '0 9,10,13,17,19 * * *',
     handler: async () => {
-      await specialTaskReport(1144, "phone");
-      await specialTaskReport(1144, "electricity");
+      await specialTaskReport(1144, 'electricity');
     },
   },
 ];
